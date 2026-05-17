917ce216b1f725c0707a8b34dcd6f3978841bbe2
jeongminju <wjdalswn0405@naver.com>
2026-05-17 22:34:07 +0900

    feat: 수료 시도횟수, 성공 횟수, 실패 횟수 담은 학생 클래스 추가

---
+++ src/main/java/org/example/person/student/Student.java
@@ -0,0 +1,36 @@
+package org.example.person.student;
+
+import org.example.person.Person;
+
+public class Student extends Person {
+    private int attempts;
+    private int passCount;
+    private int failCount;
+
+    public Student(String name) {
+        super(name);
+    }
+
+    public void recordPass() {
+        attempts++;
+        passCount++;
+    }
+
+    public void recordFail() {
+        attempts++;
+        failCount++;
+    }
+
+    public void printFinalResult() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "================================\n" +
+                " " + getName() + "님의 학습 기록\n" +
+                " 총 도전 횟수: " + attempts + "번\n" +
+                " 통과: " + passCount + "번 / 실패: " + failCount + "번\n" +
+                "================================";
+    }
+}
