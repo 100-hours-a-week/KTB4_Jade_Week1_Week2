b2efea81577e8d557b0c3f0b06620ab583608e6f
jeongminju <wjdalswn0405@naver.com>
2026-05-17 20:17:35 +0900

    add: 시스템 시작시킬 main 클래스 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.system.Controller;
+import org.example.system.IoView;
+
 public class Main {
     public static void main(String[] args) {
+       String userName = IoView.inputName();
+       Controller controller = new Controller(userName);
 
+       controller.run();
     }
 }
