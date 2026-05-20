179f24a9af4607304190d66aba245c411ea2e2a9
jeongminju <wjdalswn0405@naver.com>
2026-05-20 18:29:03 +0900

    refactor: RandomNumberUtil 유틸리티 클래스 인스턴스화 및 상속 방지

--- src/main/java/org/example/system/RandomNumberUtil.java
+++ src/main/java/org/example/system/RandomNumberUtil.java
@@ -2,7 +2,11 @@ package org.example.system;
 
 import java.util.Random;
 
-public class RandomNumberUtil {
+public final class RandomNumberUtil {
+
+    private RandomNumberUtil() {
+    }
+
     public static int generate(int min, int max) {
         return new Random().nextInt(max - min + 1) + min;
     }
