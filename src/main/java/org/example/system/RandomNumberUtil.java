178cb6a2faede9e6b0137d45ae4102757c497a49
jeongminju <wjdalswn0405@naver.com>
2026-05-24 03:02:15 +0900

    style: RandomNumberUtil 생성자 포맷 정리

--- src/main/java/org/example/system/RandomNumberUtil.java
+++ src/main/java/org/example/system/RandomNumberUtil.java
@@ -4,8 +4,7 @@ import java.util.Random;
 
 public final class RandomNumberUtil {
 
-    private RandomNumberUtil() {
-    }
+    private RandomNumberUtil() {}
 
     public static int generate(int min, int max) {
         return new Random().nextInt(max - min + 1) + min;
