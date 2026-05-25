3b7d838e351ffab95233345044ae78858e4699c5
jeongminju <wjdalswn0405@naver.com>
2026-05-25 22:17:42 +0900

    feat: 트랙이름과 골인시간 정보 담을 dto 추가

--- src/main/java/org/example/task/race/RaceInfoDto.java
+++ src/main/java/org/example/task/race/RaceInfoDto.java
@@ -1,7 +1,8 @@
 package org.example.task.race;
 
-public record RaceInfoDto (
-        String trackName,
-        int lapsedTime
-) {
+public record RaceInfoDto(String trackName, int lapsedTime) {
+    @Override
+    public String toString() {
+        return trackName + " (" + lapsedTime + "초)";
+    }
 }
