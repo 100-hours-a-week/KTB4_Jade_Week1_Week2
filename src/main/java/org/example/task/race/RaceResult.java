7fc150fd85c6c8fe29652bab03778b472172b72e
jeongminju <wjdalswn0405@naver.com>
2026-05-25 22:18:15 +0900

    feat: 우승자와 사용자 예측 비교하는 로직 추가 feat: 랭킹 출력하는 함수 추가

--- src/main/java/org/example/task/race/RaceResult.java
+++ src/main/java/org/example/task/race/RaceResult.java
@@ -1,12 +1,24 @@
 package org.example.task.race;
 
+import org.example.system.io.Message;
+import org.example.track.TrackType;
+
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class RaceResult {
-    private ConcurrentLinkedQueue<RaceInfoDto> rankingQueue;
+    private final ConcurrentLinkedQueue<RaceInfoDto> rankingQueue;
 
-    public RaceResult(ConcurrentLinkedQueue<RaceInfoDto> rankingQueue) {
-        this.rankingQueue = rankingQueue;
+    public RaceResult() {
+        this.rankingQueue = new ConcurrentLinkedQueue<>();
+    }
+
+    public boolean matchWinner(String predict) {
+        String predictedTrack = TrackType.from(predict).getName();
+        RaceInfoDto winner = rankingQueue.peek();
+        if (winner == null) {
+            throw new IllegalStateException(Message.NON_RACE_RESULT);
+        }
+        return winner.trackName().equals(predictedTrack);
     }
 
     public void recordRanking(RaceInfoDto raceInfoDto) {
@@ -14,7 +26,11 @@ public class RaceResult {
     }
 
     public void printRanking() {
-
+        int rank = 1;
+        for (RaceInfoDto dto : rankingQueue) {
+            System.out.println(rank + "등: " + dto);
+            rank++;
+        }
     }
 
 }
