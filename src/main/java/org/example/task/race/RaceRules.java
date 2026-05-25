341e71fbaa155a68d2b53c0372eee5fefa639fac
jeongminju <wjdalswn0405@naver.com>
2026-05-26 01:21:28 +0900

    refactor: 변수명 수정

--- src/main/java/org/example/task/race/RaceRules.java
+++ src/main/java/org/example/task/race/RaceRules.java
@@ -1,9 +1,9 @@
 package org.example.task.race;
 
 public final class RaceRules {
-    public static final int MIN_MOVE_DISTANCE = 1;
-    public static final int MAX_MOVE_DISTANCE = 3;
-    public static final long GOAL_TIME_SECONDS = 10L;
+    public static final int MIN_MOVE = 2;
+    public static final int MAX_MOVE = 5;
+    public static final int GOAL_DISTANCE = 10;
 
     private RaceRules() {
     }
