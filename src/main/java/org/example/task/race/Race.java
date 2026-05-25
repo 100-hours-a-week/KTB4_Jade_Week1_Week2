dc4769d801b510277b2285e5db2b0c272f124008
jeongminju <wjdalswn0405@naver.com>
2026-05-25 22:24:46 +0900

    feat: 경주 실행 흐름 구현

--- src/main/java/org/example/task/race/Race.java
+++ src/main/java/org/example/task/race/Race.java
@@ -1,30 +1,92 @@
 package org.example.task.race;
 
+import org.example.system.io.InputOutputView;
+import org.example.system.io.OutputView;
+import org.example.system.util.ValidateUtil;
 import org.example.task.TaskStrategy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Race implements TaskStrategy {
     private List<Runner> runners;
     private RaceResult raceResult;
     private static final Long GOAL_TIME = 10L;
+    private static final int MIN_ARRANGE = 1;
+    private static final int MAX_ARRANGE = 3;
+    private static final String VALID_PREDICTION_INPUT = "1, 2, 3 중 하나를 입력하세요.";
 
-    public Race(List<Runner> runners, RaceResult raceResult) {
-        this.runners = runners;
-        this.raceResult = raceResult;
+    public Race(List<String> runnersName) {
+        this.raceResult = new RaceResult();
+        this.runners = runnersName.stream()
+                .map(name -> new Runner(name, raceResult))
+                .toList();
     }
 
     @Override
     public boolean run() {
-        //Runner 클래스 생성(Full/Ai/Cloud)
-        //레이싱 시작시키기
-        //최종 순위 출력 함수 호출.
+        String predict = getPredict();
+        racing();
+        printRanking();
+        return raceResult.matchWinner(predict);
+    }
+
+    private void racing() {
+        List<Thread> threads = createThreads();
+        waitForFinish(threads);
+    }
+
+    private List<Thread> createThreads() {
+        List<Thread> runnersThreads = new ArrayList<>();
+
+        for (Runner runner : runners) {
+            Thread thread = new Thread(runner);
+            runnersThreads.add(thread);
+            thread.start();
+        }
+
+        return runnersThreads;
+    }
+
+    private void waitForFinish(List<Thread> threads) {
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private void printRanking() {
+        raceResult.printRanking();
+    }
+
+    private String getPredict() {
+        while (true) {
+            try {
+                String input = InputOutputView.inputRacePredict();
+                int number = ValidateUtil.validateNumeric(input);
+                validatePredict(number);
+                return input;
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e.getMessage());
+            }
+        }
+    }
+
+    private void validatePredict(int number) {
+        if (number < MIN_ARRANGE || number > MAX_ARRANGE) {
+            throw new IllegalArgumentException(VALID_PREDICTION_INPUT);
+        }
     }
 
     @Override
     public String toString() {
-        //소개글 작성
-        return "";
+        return "================================\n" +
+                " 카테부 트랙 달리기 경주! 1등 트랙을 예측하세요! (숫자 입력)\n" +
+                " (1.Cloud / 2.AI / 3.FullStack)\n" +
+                "================================";
     }
 
     @Override
