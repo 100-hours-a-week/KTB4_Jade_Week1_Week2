5f02d1ce809ba9689074bae8746a8a872f19d5bb
jeongminju <wjdalswn0405@naver.com>
2026-05-26 01:23:04 +0900

    feat: Runner 클래스 구현

--- src/main/java/org/example/task/race/Runner.java
+++ src/main/java/org/example/task/race/Runner.java
@@ -1,9 +1,12 @@
 package org.example.task.race;
 
+import org.example.system.util.RandomNumberUtil;
+
 public class Runner implements Runnable {
     private final String trackName;
-    private long startRaceTime;
     private final RaceResult raceResult;
+    private static final long ONE_SECOND_MILLIS = 1000L;
+    private long startTime;
 
     public Runner(String trackName, RaceResult raceResult) {
         this.trackName = trackName;
@@ -12,7 +15,57 @@ public class Runner implements Runnable {
 
     @Override
     public void run() {
-        //최종 목표 시간이 다 될 때까지 1~3초 단위의 랜덤 숫자 계속 할당받음
-        //최종 목표 시간이 다 되었다면 Race에 순위 등록 후 "트랙name" + 도착~! 출력.
+        startTime = System.currentTimeMillis();
+        race();
+        finish();
+    }
+
+    private void race() {
+        int nowDistance = 0;
+
+        while (nowDistance < RaceRules.GOAL_DISTANCE) {
+            waitOneSecond();
+
+            int randomMove = generateMoveDistance();
+            nowDistance = move(nowDistance, randomMove);
+
+            printCurrentDistance(nowDistance);
+        }
+    }
+
+    private void waitOneSecond() {
+        try {
+            Thread.sleep(ONE_SECOND_MILLIS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IllegalStateException(trackName + " 경주가 중단되었습니다.", e);
+        }
+    }
+
+    private int generateMoveDistance() {
+        return RandomNumberUtil.generate(
+                RaceRules.MIN_MOVE,
+                RaceRules.MAX_MOVE
+        );
+    }
+
+    private int move(int nowDistance, int moveDistance) {
+        return Math.min(
+                nowDistance + moveDistance,
+                RaceRules.GOAL_DISTANCE
+        );
+    }
+
+    private void printCurrentDistance(int nowDistance) {
+        System.out.println(trackName + ": " + nowDistance + "/" + RaceRules.GOAL_DISTANCE);
+    }
+
+    private void finish() {
+        raceResult.recordRanking(new RaceInfoDto(trackName, (int)getElapsedSeconds(startTime)));
+        System.out.println("⛳️ " + trackName + " 도착~!");
+    }
+
+    private long getElapsedSeconds(long startTime) {
+        return (System.currentTimeMillis() - startTime) / 1000;
     }
 }
